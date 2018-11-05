f6f4d0449b0c1e7d7e57cc5a08b0e5ef80b5d69a
chaitanyarajugithub <chaitanyaraju2@gmail.com>
2018-11-05 15:16:12 +0530

    bat file changes

--- Batch.bat
+++ Batch.bat
@@ -1,9 +1,9 @@
-set projectLocation=D:\Chaitanya_Work\TESTINGSOFTWARES\WorkSpaceSapling\TestNG_Jenkins
+set projectLocation=D:\Chaitanya_Work\Githubrepo\TestNG_Jenkins
 
-cd D:\Chaitanya_Work\TESTINGSOFTWARES\WorkSpaceSapling\TestNG_Jenkins
+cd D:\Chaitanya_Work\Githubrepo\TestNG_Jenkins
 
-set classpath=D:\Chaitanya_Work\TESTINGSOFTWARES\WorkSpaceSapling\TestNG_Jenkins\bin\;D:\Chaitanya_Work\TESTINGSOFTWARES\WorkSpaceSapling\TestNG_Jenkins\lib\*
+set classpath=D:\Chaitanya_Work\Githubrepo\TestNG_Jenkins\bin\;D:\Chaitanya_Work\Githubrepo\TestNG_Jenkins\lib\*
 
-java org.testng.TestNG D:\Chaitanya_Work\TESTINGSOFTWARES\WorkSpaceSapling\TestNG_Jenkins\testng.xml
+java org.testng.TestNG D:\Chaitanya_Work\Githubrepo\TestNG_Jenkins\testng.xml
 
 pause
